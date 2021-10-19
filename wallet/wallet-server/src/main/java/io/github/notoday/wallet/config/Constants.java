227e00cde3ea2bed085bb13ca34c0264979b1ef9
notoday <no.today@outlook.com>
2021-10-19 15:28:25 +0800

    Initial version of wallet generated by generator-jhipster@7.2.0

---
+++ wallet/wallet-server/src/main/java/io/github/notoday/wallet/config/Constants.java
@@ -0,0 +1,11 @@
+package io.github.notoday.wallet.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM = "system";
+
+    private Constants() {}
+}
---
+++ wallet/wallet-server/src/main/java/io/github/notoday/wallet/repository/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data JPA repositories.
+ */
+package io.github.notoday.wallet.repository;
